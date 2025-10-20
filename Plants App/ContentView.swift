d00587fcbde1c486b965e0500e334c28c9bbdbe1
Fajer <fajer.alqahtani20@gmail.com>
2025-10-20 09:59:00 +0300

    Initial Commit

---
+++ Plants App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plants App
+//
+//  Created by Fajer alQahtani on 28/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plants App/Plants_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plants_AppApp.swift
+//  Plants App
+//
+//  Created by Fajer alQahtani on 28/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Plants_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
