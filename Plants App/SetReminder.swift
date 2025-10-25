b0032ad0d8f14b06d4c2c9a834b9b05cf0f09838
Fajer <fajer.alqahtani20@gmail.com>
2025-10-25 23:26:58 +0300

    First and second page

---
+++ Plants App/SetReminder.swift
@@ -0,0 +1,18 @@
+//
+//  SetReminder.swift
+//  Plants App
+//
+//  Created by Fajer alQahtani on 30/04/1447 AH.
+//
+
+import SwiftUI
+
+struct SetReminder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetReminder()
+}
